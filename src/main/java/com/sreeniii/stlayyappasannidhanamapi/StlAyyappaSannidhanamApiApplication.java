25b4e84c9cec7388829a9e80dfbf41368143240a
sreeniii <sreenivaskanthamneni@gmail.com>
2018-12-13 18:52:02 -0600

    First Commit

---
+++ src/main/java/com/sreeniii/stlayyappasannidhanamapi/StlAyyappaSannidhanamApiApplication.java
@@ -0,0 +1,14 @@
+package com.sreeniii.stlayyappasannidhanamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StlAyyappaSannidhanamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StlAyyappaSannidhanamApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/sreeniii/stlayyappasannidhanamapi/StlAyyappaSannidhanamApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sreeniii.stlayyappasannidhanamapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StlAyyappaSannidhanamApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
